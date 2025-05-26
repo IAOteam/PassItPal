b803edb9af62bd90c5a8296a223deb23330111e2
mokshanirgutti <mokshasai910@gmail.com>
2025-05-26 14:50:49 +0530

    frontend-basix

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import { NavBar } from "./components/nav/NavBar"
+import HeroSection from "./components/pages/landing/HeroSection"
+
+function App() {
+  return (
+    <div className="pagePadding">
+    
+    <NavBar/>
+    
+    <HeroSection/>
+    
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/src/assets/index.ts
@@ -0,0 +1,1 @@
+export { ReactComponent as SharingLogo } from './sharing.svg?react';

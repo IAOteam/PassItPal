02a0628a5bce41280551fc57d7534b9065bb5f19
mokshanirugutti <mokshasai910@gmail.com>
2025-06-06 19:27:15 +0530

    pass-listings-route

--- frontend/src/components/footer/Footer.tsx
+++ frontend/src/components/footer/Footer.tsx
@@ -5,7 +5,7 @@ const Footer: React.FC = () => {
     <div className="pagePadding">
         <div className="flex justify-between items-center h-96">
           <div className="flex items-center">
-            <img src="/logo.png" alt="Logo" className="w-52 h-52" />
+            <img src="/logo.png" alt="Logo" className="w-32 h-32" />
           </div>
           <div className="flex items-center">
             <p>Copyright 2025 PassItPal</p>
--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -10,3 +10,37 @@ export interface User {
   };
 
 
+  export interface Listing {
+    _id: string;
+    seller: {
+      _id: string;
+      username: string;
+      email: string;
+      mobileNumber: string;
+      role: string;
+      profilePictureUrl: string;
+    };
+    cultPassType: string;
+    expiryDate: string;
+    askingPrice: number;
+    originalPrice: number;
+    availableCredits: number;
+    city: string;
+    latitude: number;
+    longitude: number;
+    adImageUrl: string;
+    isAvailable: boolean;
+    isPromoted: boolean;
+    createdAt: string;
+    updatedAt: string;
+  }
+
+export interface ListingsResponse {
+    message: string;
+    listings: Listing[];
+    totalCount: number;
+    currentPage: number;
+    totalPages: number;
+    limit: number;
+  }
+  

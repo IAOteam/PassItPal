d206bb58ce1cd63f1524740073631b901e05e25a
mokshanirgutti <mokshasai910@gmail.com>
2025-07-15 01:43:17 +0530

    updating-lading-page

--- frontend/src/components/pages/landing/HomePage.tsx
+++ frontend/src/components/pages/landing/HomePage.tsx
@@ -1,23 +1,27 @@
 // HomePage.tsx
-import React, { useRef } from 'react';
-import HeroSection from '@/components/pages/landing/HeroSection';
-import CategoryTabs from '@/components/pages/landing/CategoryTabs';
-import FeaturedContent from './FeaturedContent';
-import TrendingListings from '@/components/listings/TrendingListings';
-import HowItWorks from './HowItWorks';
-import Testimonials from './Testimonials';
-import Achievements from './Achievements';
+import React, { useRef } from "react";
+import HeroSection from "@/components/pages/landing/HeroSection";
+// import CategoryTabs from "@/components/pages/landing/CategoryTabs";
+import FeaturedContent from "./FeaturedContent";
+import TrendingListings from "@/components/listings/TrendingListings";
+import HowItWorks from "./HowItWorks";
+import Testimonials from "./Testimonials";
+import Achievements from "./Achievements";
 
 const HomePage: React.FC = () => {
   const categoryRef = useRef<HTMLDivElement | null>(null);
 
   return (
     <div>
-      <HeroSection scrollToCategory={() => categoryRef.current?.scrollIntoView({ behavior: 'smooth' })} />
+      <HeroSection
+        scrollToCategory={() =>
+          categoryRef.current?.scrollIntoView({ behavior: "smooth" })
+        }
+      />
       <div ref={categoryRef}>
-        <CategoryTabs />
+        {/* <CategoryTabs /> */}
+        <FeaturedContent />
       </div>
-      <FeaturedContent />
       <TrendingListings />
       <Achievements />
       <HowItWorks />

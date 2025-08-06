b15ac2ed3077062369216ed862250c6446ce1a07
Vara kare <105549052+VaraKare@users.noreply.github.com>
2025-08-06 22:31:24 +0530

    Update jest.config.cjs

--- frontend/jest.config.cjs
+++ frontend/jest.config.cjs
@@ -24,8 +24,8 @@ module.exports = {
   roots: ['<rootDir>/src'],
   
   testMatch: [
-    // "**/__tests__/**/*.+(ts|tsx|js)",
-    // "**/?(*.)+(spec|test).+(ts|tsx|js)"
+    "**/__tests__/**/*.+(ts|tsx|js)",
+    "**/?(*.)+(spec|test).+(ts|tsx|js)"
   ],
 
   testPathIgnorePatterns: [
